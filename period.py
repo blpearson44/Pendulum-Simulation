5702f2f3f6ba0fa6884d5f6ceab4836e8a09785f
Ben Pearson <blpearson44@icloud.com>
2021-09-15 22:26:33 -0700

    Period functions are now working, and ideal period has been set

--- period.py
+++ period.py
@@ -1,5 +1,5 @@
 # Written by Ben Pearson
-# V 0.1.0
+# V 1.0.0
 
 # Modules
 import numpy as np
@@ -11,15 +11,29 @@ G = 9.8
 PI = np.pi
 
 # This function recursively calculates the sum in K(x)
-def series_sum(x, n=1, total=0.0):
-    if n > 3:
+def series_sum(x, n=1, total=1, most=3):
+    if n > most:
         return total
     else:
-        return series_sum(x, n + 1, ((2 * n + 1)/(2 * n + 2))**2 * x**2 * total)
+        return series_sum(x, n + 1, ((2 * n + 1)/(2 * n + 2))**2 * x**2 * total, most)
 
 # Function to calculate k(x) from the assignment by calling on series_sum
-def k(x):
+def k(x, most=10):
     total = x**2 / 4
-    return PI / 2 * (1 + series_sum(x, 1, total))
-    
-print(k(2))
+    return PI / 2 * (1 + series_sum(x, 1, total, most))
+
+# Function to calculate period from equation given
+def period(angle, most):
+    return 4 * (L/G)**(1 / 2) * k(np.sin(angle/2), most)
+
+angle_max = [PI/8, PI/4, 3*PI/8, PI/2]
+
+# Testing purposes
+j = 1
+for i in angle_max:
+    print(j)
+    j += 1
+    print(period(i, 1))
+    print(period(i, 10))
+    print(period(i, 100))
+    print(period(i, 200))
